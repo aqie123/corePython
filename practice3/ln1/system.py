daa2fb4322700d80d4e0b970e9790bfe4478380c
aqie123 <2924811900@qq.com>
2018-04-03 16:32:04 +0800

    finish env prepare

---
+++ practice3/ln1/system.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+print("hello world 啊切")
+print(bin(10))  # 十进制 转二进制 0b1001
+print(bin(0o7)) # 八进制 转二进制 0b111
+print(bin(0xE)) # 十六进制转二进制 0b1110
+
+print(int(0b111)) # 二进制转十进制
+
+print(hex(888)) # 十进制转十六进制
+
+print(oct(0b111)) # 二进制转八进制
